e524310c67934975ff9f37271ec48661dcb6418d
elokwentnie <nszczepanek@op.pl>
2024-07-30 00:06:11 +0200

    Added extraction of metadata from image

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
             'heic_to_jpg=src.image_processing.heic_to_jpg:main',
             'reduce_img_size=src.image_processing.reduce_img_size:main',
             'remove_background=src.image_processing.remove_background:main',
+            'extract_img_metadata=src.image_processing.extract_img_metadata:main',
             'merge_pdf=src.file_management.merge_pdf:main',
             'doc_to_pdf=src.file_management.doc_to_pdf:main',
             'pdf_to_doc=src.file_management.pdf_to_doc:main',
